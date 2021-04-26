204a7caf1895314ffbd264280f9ce4e39925f399
DanaLearnsToCode <77691366+DanaLearnsToCode@users.noreply.github.com>
2021-04-26 09:31:44 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,9 +22,13 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["question1"]=request.form['question1']
-    firstAnswer = session["question1"]
+    session["question1"]=request.form['question1'] 
     return render_template('page2.html')
+
+@app.route('/page3',methods=['GET','POST'])
+def renderPage3():
+    session["question2"]=request.form['question2']
+    return render_template('page3.html')
 
 
 
